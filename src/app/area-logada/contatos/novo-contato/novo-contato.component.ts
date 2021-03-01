5399e0f5427e1a293c8d47733619b201984a484d
kelvinbass <kelbass17@gmail.com>
2021-03-01 11:16:04 -0300

    Adicionando formularios agrupados

--- src/app/area-logada/contatos/novo-contato/novo-contato.component.ts
+++ src/app/area-logada/contatos/novo-contato/novo-contato.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscriber } from 'rxjs';
 import { ContatoService } from '../contatos.service';
@@ -23,9 +23,11 @@ export class NovoContatoComponent implements OnInit {
     this.contatoForm = this.formBuilder.group({
       nome: ['', Validators.required],
       cpf: ['', [Validators.required, Validators.minLength(11)]],
-      banco: ['', Validators.required],
-      ag: ['', [Validators.required, Validators.minLength(4)]],
-      cc: ['', [Validators.required, Validators.minLength(5)]],
+      dadosBancarios: this.formBuilder.group({
+        banco: ['', Validators.required],
+        ag: ['', [Validators.required, Validators.minLength(4)]],
+        cc: ['', [Validators.required, Validators.minLength(5)]],
+      }),
     });
   }
 
@@ -37,39 +39,44 @@ export class NovoContatoComponent implements OnInit {
     return this.contatoForm.get(nomeControle).invalid && this.contatoForm.get(nomeControle).touched;
   }
 
-  validateAllFormFields() {
-    Object.keys(this.contatoForm.controls).forEach(field => {
-      const control = this.contatoForm.get(field);
-      control.markAsTouched();
+  validateAllFormFields(form: FormGroup) {
+    Object.keys(form.controls).forEach(field => {
+      const control = form.get(field);
+      if (control instanceof FormControl) {
+        control.markAsTouched();
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+        
     }) //função para validar vaios campos so mesmo tempo
   }
 
   onSubmit() {
     if (this.contatoForm.invalid) {
-      this.validateAllFormFields();
+      this.validateAllFormFields(this.contatoForm);
       return;
     }
-  
+
     this.salvarContato();
   }
-    
+
   salvarContato() {
     this.contatoForm.value
-      this.contatoService.createContato(this.contatoForm.value)
+    this.contatoService.createContato(this.contatoForm.value)
       .subscribe(
         response => this.onSuccessSalvarContato(),
         error => this.onErrorSalvarContato(),
       )
-    }  
+  }
 
-    onSuccessSalvarContato(){
-      alert('Deu bom, contato salvo com sucesso!');
-      this.router.navigate(['contatos']);
-    }
+  onSuccessSalvarContato() {
+    alert('Deu bom, contato salvo com sucesso!');
+    this.router.navigate(['contatos']);
+  }
 
-    onErrorSalvarContato() {
-      alert('Deu ruim, salvou não pai!');
-    }
+  onErrorSalvarContato() {
+    alert('Deu ruim, salvou não pai!');
+  }
 }
 
 
